1e5421dc6cf3b0df319441be4816ad83e47e07ee
Johnathan brown <john1234brown23@gmail.com>
2024-08-20 23:01:37 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -1,2 +1,21 @@
-const { app } = require('express');
-GLOBALLY.setExpressApp(app);
+const express = require('express');
+const app = express();
+const isExpressSymbol = GLOBALLY.getGlobalSymbol();
+app[isExpressSymbol] = true;
+
+GLOBALLY.setExpressApp(app, GLOBALLY);
+//Make sure to ^ Set your expressApp via the globally to attach middlewares properly! Obviously still utilize helmet and cors!
+//Make sure to utilize the following here!
+process.child.on(GLOBALLY.getGlobalString(), ()=>{
+    console.log('Shutting down Server from parent before timeout!');
+    app.removeAllListeners();
+    listen.closeAllConnections();
+    listen.close();
+    setTimeout(() =>{
+        console.log('Shutting Down Server from parent message to stop after timeout!');
+    },10);
+})
+
+
+app.use('/', express.static(path.join(process.cwd, 'public')));
+const listen = app.listen(3001);
--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const { GodBox } = require('../index.js');
 try {
-const test = new GodBox('./server.js', './test/public', 60000, 15000, false);
+const test = new GodBox('./server.js', './test/public', 60000, 1000, false);
 
 }catch(err){
  console.log(err);
